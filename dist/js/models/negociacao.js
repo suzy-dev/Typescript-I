4c41bbc89ddef8d833f9abd0bb1407e50e42ed61
Suzana <suzanasouza.caetano@gmail.com>
2023-12-22 23:02:54 -0300

    propriedades privadas com #

---
+++ dist/js/models/negociacao.js
@@ -0,0 +1,14 @@
+class Negociacao {
+
+    #data
+    #quantidade
+    #valor
+
+    constructor(data, quantidade, valor){
+        this.#data = data
+        this.#quantidade = quantidade
+        this.#valor = valor
+    }
+}
+
+export default Negociacao

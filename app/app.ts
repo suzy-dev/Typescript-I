8d9d28cebc9b616a04cc0f01d5e3436f95057af8
Suzana <suzanasouza.caetano@gmail.com>
2024-01-09 20:17:19 -0300

    create script npm run watch

--- app/app.ts
+++ app/app.ts
@@ -1,6 +1,6 @@
 import Negociacao from './models/negociacao.js'
 
-const negociacao = new Negociacao(new Date(), 2)
+const negociacao = new Negociacao(new Date(), 2, 100)
 
 console.log(negociacao.volume)
 console.log(negociacao.valor)

321b11b3df1499251b1776da77558a3531b228ce
Suzana <suzanasouza.caetano@gmail.com>
2024-01-09 20:39:45 -0300

    sintaxe private ts _ mostra que é prop priv

---
+++ dist/js/app.js
@@ -0,0 +1,6 @@
+import Negociacao from './models/negociacao.js';
+const negociacao = new Negociacao(new Date(), 2, 100);
+console.log(negociacao.volume);
+console.log(negociacao.valor);
+console.log(negociacao.data);
+console.log(negociacao.quantidade);
